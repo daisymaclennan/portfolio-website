2091e27bf8d1365b303feee63ceb7b1982d7eec2
Daisy Maclennan <daisy@sidigital.co>
2019-10-30 16:28:48 +0000

    First static build

---
+++ static/myWork.js
@@ -0,0 +1,65 @@
+const myWork = [
+  {
+    title: 'Word muncher',
+    slug: 'word-muncher',
+    description: 'Word Muncher is a rich text editor made using React, Next.js and Draft.js. The purpose of this project was to create a note app which removes all the unessacary tools you get when using programs such as Google Docs while still maintaining the professional appearance you can achieve with them.',
+    time_period: 'October 2019',
+    img: [
+      {
+        index: 0,
+        alt: 'Word processing section of the application',
+        filename: 'word-one.png'
+      },
+      {
+        index: 1,
+        alt: 'Sign in section of the application with an error visible',
+        filename: 'word-two.png'
+      },
+      {
+        index: 2,
+        alt: 'Register section of the application with an error visible',
+        filename: 'word-three.png'
+      },
+      {
+        index: 3,
+        alt: 'Register section without errors',
+        filename: 'word-four.png'
+      },
+    ]
+  },
+  {
+    title: 'Creative Portsmouth',
+    slug: 'creative-portsmouth',
+    description: 'Creative Portsmouth is a geolocation app designed to showcase the creative and cultural organisations around the city of Portsmouth.',
+    time_period: 'June 2019 to August 2019',
+    img: [
+      {
+        index: 0,
+        alt: 'Sign in box for Creative Portsmouth',
+        filename: 'cp-one.png'
+      },
+      {
+        index: 1,
+        alt: 'Home page of Creative Portsmouth',
+        filename: 'cp-two.png'
+      },
+      {
+        index: 2,
+        alt: 'Home page with new map colours for Creative Portsmouth',
+        filename: 'cp-three.png'
+      },
+      {
+        index: 3,
+        alt: 'Homepage with organisation tab open for Creative Portsmouth',
+        filename: 'cp-four.png'
+      },
+      {
+        index: 4,
+        alt: 'Homepage with organisation tab open for Creative Portsmouth',
+        filename: 'cp-five.png'
+      },
+    ]
+  }
+]
+
+export default myWork
---
+++ static/qualifications.js
@@ -0,0 +1,64 @@
+const qualifications = {
+  gcses: [
+    {
+      title: 'Mathematics',
+      grade: 7
+    },
+    {
+      title: 'Computer Science',
+      grade: 7
+    },
+    {
+      title: 'Biology',
+      grade: 7
+    },
+    {
+      title: 'Statistics',
+      grade: 'B'
+    },
+    {
+      title: 'Graphic Products',
+      grade: 'A'
+    },
+    {
+      title: 'Food Preparation and Nutrition',
+      grade: 6
+    },
+    {
+      title: 'General Studies',
+      grade: 7
+    },
+    {
+      title: 'Chemistry',
+      grade: 5
+    },
+    {
+      title: 'English Language',
+      grade: 6
+    },
+    {
+      title: 'English Literature',
+      grade: 5
+    },
+    {
+      title: 'Physics',
+      grade: 4
+    },
+    {
+      title: 'Art & Design',
+      grade: 5
+    },
+  ],
+  software: [
+    {
+      title: 'BCS Level 3 Certificate in Programming',
+      grade: 'PASS'
+    },
+    {
+      title: 'BCS Level 3 Certificate in Software Development Context and Methodologies',
+      grade: 'PASS'
+    },
+  ]
+}
+
+export default qualifications
